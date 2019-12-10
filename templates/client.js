3fa00cd40ef8b71af1912d64b37d762c1eba855f
DEKKERS <saindeepak799@gmail.com>
2019-12-10 12:58:00 +0530

    Add files via upload

---
+++ templates/client.js
@@ -0,0 +1,16 @@
+var el = x => document.getElementById(x);
+
+function showPicker(inputId) { el('file-input').click(); }
+
+function showPicked(input) {
+    el('upload-label').innerHTML = input.files[0].name;
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        el('image-picked').src = e.target.result;
+        el('image-picked').className = '';
+    }
+    reader.readAsDataURL(input.files[0]);
+}
+
+
+
